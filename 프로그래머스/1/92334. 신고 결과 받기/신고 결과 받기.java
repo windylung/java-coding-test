0dc36750a8e0856ffc3123d8178f925904cd8ff9
이지수 <jisoolee031@gmail.com>
2024-11-18 20:18:34 +0900

    [level 1] Title: 신고 결과 받기, Time: 147.09 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+
+class Solution {
+    public static int[] solution(String[] id_list, String[] report, int k) {
+        int[] result = new int[id_list.length];
+        HashMap<String, Integer> id = new HashMap<>();
+        ArrayList<HashMap<String, Integer>> mapList = new ArrayList<>();
+        for (int i = 0; i < id_list.length; i++) {
+            id.put(id_list[i], i);
+            mapList.add(new HashMap<>());
+        }
+
+        for (int i = 0; i < report.length; i++) {
+            String[] line = report[i].split(" ");
+            // 신고당한 ID
+            Integer a = id.get(line[1]);
+            HashMap<String, Integer> map = mapList.get(a);
+
+            // 신고한 ID와 신고 횟수
+            if (map.containsKey(line[0])) {
+                map.put(line[0], map.get(line[0]) + 1);
+            } else {
+                map.put(line[0], 1);
+            }
+        }
+
+
+        for (int i = 0 ; i < id_list.length ; i++) {
+            int count = (int) mapList.get(i).values().stream().count();
+            if (count >= k){
+                Set<String> reporters = mapList.get(i).keySet();
+                for (String reporter : reporters) {
+                    Integer idx = id.get(reporter);
+                    result[idx] += 1;
+                }
+            }
+        }
+        return result;
+    }
+}
